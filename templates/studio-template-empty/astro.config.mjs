9e787332ed9f3bd3421ff046aea118050a7fbee7
Elian Van Cutsem <hello@elian.codes>
2024-03-05 22:01:00 +0100

    chore: upgrade empty to latest

--- templates/studio-template-empty/astro.config.mjs
+++ templates/studio-template-empty/astro.config.mjs
@@ -4,10 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [db()],
-	db: {
-		studio: true,
-		collections: {
-			// define your collections here!
-		},
-	},
 });

e3149a7b20d6fd48578a38b14a705093cecf0845
Elian Van Cutsem <hello@elian.codes>
2024-03-05 22:09:55 +0100

    chore: upgrade and make blog compatible with db 0.6.0

--- templates/studio-template-blog/astro.config.mjs
+++ templates/studio-template-blog/astro.config.mjs
@@ -1,31 +1,9 @@
-import db, { NOW, defineCollection, field } from '@astrojs/db';
+import db from '@astrojs/db';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import seed from './studio.seed.ts';
-
-export const Blog = defineCollection({
-	fields: {
-		title: field.text(),
-		description: field.text(),
-		slug: field.text({ optional: true }),
-		publishedAt: field.date({ default: NOW }),
-		updatedAt: field.date({ optional: true }),
-		heroImage: field.text({
-			default: '/blog-placeholder-1.jpg',
-		}),
-		content: field.text({ multiline: true }),
-	},
-});
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [db(), sitemap()],
-	db: {
-		studio: true,
-		collections: {
-			Blog,
-		},
-		data: seed,
-	},
 });

345c4f49f96eeab41e79983c6803b8f523cf0ef0
Elian Van Cutsem <hello@elian.codes>
2024-03-05 22:49:10 +0100

    chore: ignore biome (sorry ema)

--- templates/studio-template-blog/src/pages/rss.xml.ts
+++ templates/studio-template-blog/src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
-import rss from '@astrojs/rss';
-import type { APIContext } from 'astro';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
-import { Blog, db } from 'astro:db';
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { Blog, db } from "astro:db";
 
 export async function GET(context: APIContext) {
 	const posts = await db.select().from(Blog);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
+		// biome-ignore lint/style/noNonNullAssertion:
 		site: context.site!,
 		items: posts.map((post) => ({
 			title: post.title,

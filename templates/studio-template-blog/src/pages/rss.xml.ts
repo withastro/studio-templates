db85895b68882584fb6f3bd8b192a0b7ed940c97
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-05 17:45:49 -0600

    remove biome ignore

--- templates/studio-template-blog/src/pages/rss.xml.ts
+++ templates/studio-template-blog/src/pages/rss.xml.ts
@@ -1,14 +1,13 @@
-import rss from "@astrojs/rss";
-import type { APIContext } from "astro";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
-import { Blog, db } from "astro:db";
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { Blog, db } from 'astro:db';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context: APIContext) {
 	const posts = await db.select().from(Blog);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		// biome-ignore lint/style/noNonNullAssertion:
 		site: context.site!,
 		items: posts.map((post) => ({
 			title: post.title,

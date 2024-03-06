b754d8bbc1d7cf8a316504b7128ef66be4925c78
Nate Moore <nate@astro.build>
2024-03-06 16:44:43 -0600

    normalize waitlist config files

--- templates/studio-template-waitlist/db/config.ts
+++ templates/studio-template-waitlist/db/config.ts
@@ -1,6 +1,12 @@
-import { Signup } from './collections';
-import { defineDB } from 'astro:db';
+import { defineDB, defineTable, column } from 'astro:db';
 
+export const Signup = defineTable({
+	columns: {
+		email: column.text({ unique: true }),
+	},
+});
+
+// https://astro.build/db/config
 export default defineDB({
 	tables: {
 		Signup,

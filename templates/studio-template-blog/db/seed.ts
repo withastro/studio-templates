fffd1d38d500bb6806dee97a691e4034fc1eb3bd
Nate Moore <nate@astro.build>
2024-03-06 16:44:30 -0600

    normalize blog config files

--- templates/studio-template-blog/db/seed.ts
+++ templates/studio-template-blog/db/seed.ts
@@ -1,42 +1,46 @@
+import { db, Blog } from 'astro:db';
 import { readFile } from 'node:fs/promises';
-import { Blog, db } from 'astro:db';
 
-const lipsum = await readFile(new URL('../assets/lipsum.md', import.meta.url), 'utf8');
+// https://astro.build/db/seed
+export default async function seed() {
+	const lipsum = await readFile(new URL('../assets/lipsum.md', import.meta.url), 'utf8');
 
-const markdownStyleGuide = await readFile(
-	new URL('../assets/markdown-style-guide.md', import.meta.url),
-	'utf8',
-);
+	const markdownStyleGuide = await readFile(
+		new URL('../assets/markdown-style-guide.md', import.meta.url),
+		'utf8',
+	);
 
-await db.insert(Blog).values([
-	{
-		title: 'Markdown Style Guide',
-		description:
-			'Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.',
-		slug: 'markdown-style-guide',
-		publishedAt: new Date('2022-07-01'),
-		heroImage: '/blog-placeholder-1.jpg',
-		content: markdownStyleGuide,
-	},
-	{
-		title: 'First post',
-		description: 'Lorem ipsum dolor sit amet',
-		publishedAt: new Date('2022-07-08'),
-		heroImage: '/blog-placeholder-3.jpg',
-		content: lipsum,
-	},
-	{
-		title: 'Second post',
-		description: 'Lorem ipsum dolor sit amet',
-		publishedAt: new Date('2022-07-15'),
-		heroImage: '/blog-placeholder-4.jpg',
-		content: lipsum,
-	},
-	{
-		title: 'Third post',
-		description: 'Lorem ipsum dolor sit amet',
-		publishedAt: new Date('2022-07-22'),
-		heroImage: '/blog-placeholder-2.jpg',
-		content: lipsum,
-	},
-]);
+	await db.insert(Blog).values([
+		{
+			title: 'Markdown Style Guide',
+			description:
+				'Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.',
+			slug: 'markdown-style-guide',
+			publishedAt: new Date('2022-07-01'),
+			heroImage: '/blog-placeholder-1.jpg',
+			content: markdownStyleGuide,
+		},
+		{
+			title: 'First post',
+			description: 'Lorem ipsum dolor sit amet',
+			publishedAt: new Date('2022-07-08'),
+			heroImage: '/blog-placeholder-3.jpg',
+			content: lipsum,
+		},
+		{
+			title: 'Second post',
+			description: 'Lorem ipsum dolor sit amet',
+			publishedAt: new Date('2022-07-15'),
+			heroImage: '/blog-placeholder-4.jpg',
+			content: lipsum,
+		},
+		{
+			title: 'Third post',
+			description: 'Lorem ipsum dolor sit amet',
+			publishedAt: new Date('2022-07-22'),
+			heroImage: '/blog-placeholder-2.jpg',
+			content: lipsum,
+		},
+	]);
+
+}

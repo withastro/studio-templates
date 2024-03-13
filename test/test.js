10b29550779d7094b3602196d194c5e6e0f4872f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-03-13 10:35:01 -0500

    minor test improvements

--- test/test.js
+++ test/test.js
@@ -1,25 +1,30 @@
-import assert from 'node:assert';
-import { existsSync, readdirSync } from 'node:fs';
-import { describe, it } from 'node:test';
+// @ts-check
+
 import { build, dev } from 'astro';
+import assert from 'node:assert';
+import { existsSync } from 'node:fs';
+import { readdir } from 'node:fs/promises';
+import { join } from 'node:path';
+import { describe, it } from 'node:test';
+import { fileURLToPath } from 'node:url';
 
-const templatesDir = './templates/';
-const templates = readdirSync(templatesDir);
+const templatesDir = join(fileURLToPath(import.meta.url), '../../templates');
 
-for (const template of templates) {
+for (const template of await readdir(templatesDir)) {
 	describe(template, () => {
-		const root = templatesDir + template;
+		const root = join(templatesDir, template);
 
 		it('builds successfully', async () => {
-			await build({ root, logLevel: 'silent' });
+			await build({ root, logLevel: 'error' });
+			const distDir = join(root, 'dist');
 			assert.ok(
-				existsSync(`${root}/dist`),
-				`Expected "dist/" directory for template "${template}", but found none.`,
+				existsSync(distDir),
+				`Expected "dist" directory for template "${template}", but ${distDir} does not exist.`,
 			);
 		});
 
 		it('runs the dev server successfully', async () => {
-			const server = await dev({ root, logLevel: 'silent' });
+			const server = await dev({ root, logLevel: 'error' });
 			const res = await fetch(`http://localhost:${server.address.port}/`);
 			assert.strictEqual(
 				res.status,
